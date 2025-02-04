16bd9e28146ab6fbf2972a85f77aa3a453c5f28e
Sona Son <sonaze28@gmail.com>
2025-02-04 16:29:31 +0900

    Set up toy project with webpack

---
+++ toy-webpack/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    filename: "main.js", // 빌드 후 번들된 파일 이름
+    path: path.resolve(__dirname, "dist"), // 빌드 파일이 저장될 경로
+    publicPath: "/", // HTML에서 자원 경로 설정
+  },
+};
